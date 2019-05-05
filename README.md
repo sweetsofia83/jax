c16ba562a89d959c873348893e6e3b0e8d4f0224
Nir Koren <nirk@liveperson.com>
2019-05-05 18:23:49 +0300

    Initial commit of demo project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to JAX DevOps London Demo Repository
+## This repo contains basic Maven project with Hello-World WAR file 
---
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -0,0 +1,10 @@
+package org.nirkoren.maven.demo;
+
+public class HelloHandler {
+
+	public String sayHello() {
+		String hello = "Hello JAX DevOps members, What do you think about this conference?";
+		return hello;
+	}
+
+}
